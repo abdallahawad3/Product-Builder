cd2ca162565209a2dcb3b44e62eb61675bed94e2
abdallahawad3 <djdgdnnxcbj@gmail.com>
2024-06-18 14:38:35 +0300

    initial commit⚙️

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+// import { v4 as uuid } from "uuid";
+
+const App = () => {
+  return (
+    <h1 className="text-red-500 p-2 bg-slate-800 text-2xl">Hello world!</h1>
+  );
+};
+
+export default App;
